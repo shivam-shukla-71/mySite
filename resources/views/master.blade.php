996abce6c57ed30d4f311fb52396e722d7486061
Shivam Shukla <79891284+xvii771@users.noreply.github.com>
2023-08-23 08:57:18 +0530

    login page, header and footer modification

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,14 +10,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
 </body>
-<script>
-    $(document).ready(function(){
-        $("button").click(function()
-        {
-            alert("all set")
-        })
-    })
-</script>
+<style>
+    .cust-login{
+        height: 550px;
+        padding: 100px;
+    }
+</style>
 </html>
